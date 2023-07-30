f4b2776174cd8db76ecca1cb4b3afc73d642a516
Maksym <maksymmaliuk98@gmail.com>
2023-07-30 14:34:37 +0300

    fix: header layout modified

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,24 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import styles from './Header.module.scss';
+import shieldIcon from '../../assets/icon_shield.svg'
+import { DateTime } from '../DateTime/DateTime';
 
 export const Header = () => {
   return (
-    <header className={styles.header}>
-      <h1>it is header</h1>
-      <Link href='/'>To home page</Link>
+    <header className={styles['header']}>
+      <div className={`${styles['header__content']} container`}>
+        <div className={styles['header__emblem']}>
+          <Image src={shieldIcon} height={50} alt={'shield'} />
+        </div>
+
+        <div className={styles['header__input']}>
+          <input id='searchInput' placeholder='search' />
+          <label htmlFor="searchInput" />
+        </div>
+
+        <DateTime />
+      </div>
     </header>
   )
 }
