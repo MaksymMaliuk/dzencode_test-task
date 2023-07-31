25fe670b13606a91648371b744ebf1b5aa200822
Maksym <maksymmaliuk98@gmail.com>
2023-07-31 20:20:08 +0300

    add: product list component added

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,37 +1,13 @@
-'use client'
-
-import { 
-  FC, 
-  useEffect, 
-  useState 
-} from "react";
-import productsFromServer from '../../products.json';
-import { ProductItem } from "@/components/Product";
-import { Product } from "@/types/Product";
+import { ProductsList } from "@/components/ProductsList/ProductsList";
+import { FC } from "react";
 
 export const metadata = {
   title: 'Blog'
 }
 
 const Products: FC = () => {
-  const [products, setProducts] = useState<Product[]>([]);
 
-  useEffect(() => {
-    setProducts(productsFromServer)
-  }, [])
-
-  return ( 
-    <>
-      <ul> 
-        {products.map((product) => {
-         
-          return (
-            <ProductItem key={product.id} product={product}/>
-          )
-        })}
-      </ul>
-    </>
-  )
+  return <ProductsList />
 }
 
 export default Products;
---
+++ src/components/ProductsList/ProductsList.tsx
@@ -0,0 +1,23 @@
+import { FC, useEffect, useState } from "react"
+import { ProductItem } from "../ProductItem/ProductItem"
+import { Product } from "@/types/Product";
+import productsFromServer from '../../products.json';
+
+export const ProductsList: FC = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    setProducts(productsFromServer)
+  }, [])
+
+  return (
+    <ul> 
+        {products.map((product) => {
+         
+          return (
+            <ProductItem key={product.id} product={product}/>
+        )
+      })}
+    </ul>
+  )
+}
